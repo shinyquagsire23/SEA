05e3575bc28c9832035e48ec1c0d97ec834ca31d
shinyquagsire23 <mtinc2@gmail.com>
2013-02-08 19:46:19 -0800

    Wrong separator...

--- src/org/zzl/minegaming/SEA/GlobalVars.java
+++ src/org/zzl/minegaming/SEA/GlobalVars.java
@@ -6,10 +6,10 @@ public class GlobalVars {
 	public static String DebugCommand = "gvbam %f";
 	public static boolean FileOpened = false;
 	public static String FileLoc = "";
-	public static String LastDir = File.pathSeparator +"home" + File.pathSeparator + "maxamillion" + File.pathSeparator + "ROM Hacking" + File.pathSeparator;
+	public static String LastDir = File.separator +"home" + File.separator + "maxamillion" + File.separator + "ROM Hacking" + File.separator;
 	public static byte[] ROM = new byte[0x2000000];
 	public static byte[] NewROM = new byte[0x2000000];
-	public static String SettingsDir = DefaultDirectory() + File.pathSeparator + "settings" + File.pathSeparator;
+	public static String SettingsDir = DefaultDirectory() + File.separator + "settings" + File.separator;
 	public static String MainDir = DefaultDirectory();
 	
 	private static String DefaultDirectory()
